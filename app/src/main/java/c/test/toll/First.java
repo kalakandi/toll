0d010319279b3dcc4d3daeffe0b68919bf210a25
Apoorv <aryan.kudesia@gmail.com>
2018-09-07 17:25:15 +0530

    Start Commit 2

--- app/src/main/java/c/test/toll/First.java
+++ app/src/main/java/c/test/toll/First.java
@@ -1,100 +1,102 @@
 package c.test.toll;
-import android.provider.SyncStateContract;
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.common.util.Strings;
-import com.paytm.pgsdk.PaytmOrder;
-import com.paytm.pgsdk.PaytmPGService;
-import com.paytm.pgsdk.PaytmPaymentTransactionCallback;
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.json.JSONObject;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+public class First extends AppCompatActivity implements PaymentResultListener {
 
 
-public class First extends AppCompatActivity implements PaytmPaymentTransactionCallback {
-
-    TextView textViewPrice;
+    private Button buttonConfirmOrder;
+    private EditText editTextPayment;
 
     @Override
-    protected void onCreate (Bundle savedInstaneState){
-        super.onCreate(savedInstaneState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
-        textViewPrice = findViewById(R.id.textViewPrice);
+        findViews();
+        listeners();
+    }
 
-        findViewById(R.id.buttonBuy).setOnClickListener(
-                generateChecksum();
-        );
+    public void findViews() {
+        buttonConfirmOrder = (Button) findViewById(R.id.buttonConfirmOrder);
+        editTextPayment = (EditText) findViewById(R.id.editTextPayment);
+    }
+
+    public void listeners() {
+
+
+        buttonConfirmOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(editTextPayment.getText().toString().equals(""))
+                {
+                    Toast.makeText(First.this, "Please fill payment", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                startPayment();
+            }
+        });
     }
 
 
-    private void generateChecksum(){
-        String txnAmount = textViewPrice.getText().toString().trim();
+    public void startPayment() {
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(Api.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+        final Activity activity = this;
 
-        Api apiService = retrofit.create(Api.class);
+        final Checkout co = new Checkout();
 
-        final Paytm paytm =new Paytm(
-                Constants.M_ID,
-                Constants.CHANNEL_ID,
-                txnAmount,
-                Constants.WEBSITE,
-                Constants.CALLBACK_URL,
+        try {
+            JSONObject options = new JSONObject();
+            options.put("name", "Razorpay Corp");
+            options.put("description", "Demoing Charges");
+            //You can omit the image option to fetch the image from dashboard
+            options.put("image", "https://rzp-mobile.s3.amazonaws.com/images/rzp.png");
+            options.put("currency", "INR");
 
+            String payment = editTextPayment.getText().toString();
 
+            double total = Double.parseDouble(payment);
+            total = total * 100;
+            options.put("amount", total);
 
-        );
+            JSONObject preFill = new JSONObject();
+            preFill.put("email", "sikander@gkmit.co");
+            preFill.put("contact", "9680224241");
 
-        call<Checksum>
-    }
+            options.put("prefill", preFill);
 
-
-    @Override
-    public void onTransactionResponse(Bundle inResponse) {
-
-    }
-
-    @Override
-    public void networkNotAvailable() {
-
-    }
-
-    @Override
-    public void clientAuthenticationFailed(String inErrorMessage) {
-
-    }
-
-    @Override
-    public void someUIErrorOccurred(String inErrorMessage) {
-
-    }
-
-    @Override
-    public void onErrorLoadingWebPage(int iniErrorCode, String inErrorMessage, String inFailingUrl) {
-
-    }
-
-    @Override
-    public void onBackPressedCancelTransaction() {
-
+            co.open(activity, options);
+        } catch (Exception e) {
+            Toast.makeText(activity, "Error in payment: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void onTransactionCancel(String inErrorMessage, Bundle inResponse) {
+    public void onPaymentSuccess(String razorpayPaymentID) {
+        Toast.makeText(this, "Payment successfully done! " + razorpayPaymentID, Toast.LENGTH_SHORT).show();
 
+    }
+
+    @Override
+    public void onPaymentError(int code, String response) {
+        try {
+            Toast.makeText(this, "Payment error please try again", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.e("OnPaymentError", "Exception in onPaymentError", e);
+        }
     }
 }
