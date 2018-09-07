2ccff75d9e1d502aa71fbfe2e6f1ee6b7e11f350
yashchandel0007 <yashchandel007@outlook.com>
2018-09-08 01:28:06 +0530

    normal

--- app/src/main/java/c/test/toll/First.java
+++ app/src/main/java/c/test/toll/First.java
@@ -4,12 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.paytm.pgsdk.PaytmPaymentTransactionCallback;
-
+/*
 public class First implements PaytmPaymentTransactionCallback {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
-    }
+
 }
+*/
